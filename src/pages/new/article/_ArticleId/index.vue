0f47eec894aeaf7fcf609742b6fe0345cf5b9fc1
yslin <s5907610@gmail.com>
2020-11-22 16:50:15 +0800

    feat(new/article): get article by router params

--- src/pages/new/article/_ArticleId/index.vue
+++ src/pages/new/article/_ArticleId/index.vue
@@ -45,12 +45,12 @@
           <b-col>
             <p>[2020 美國總統大選] 這邊不知道是要填什麼資料</p>
             <h2>
-              川普新冠肺炎染疫事件經過
+              {{ news.title }}
             </h2>
             <hr class="my-2">
             <div class="text-center text-secondary">
               <b-icon icon="clock" />
-              最後更新時間 10/14  17:35
+              最後更新時間 {{ formatTime(news.lastUpdatedAt) }}
             </div>
             <div class="d-flex justify-content-between py-3">
               <div>
@@ -68,7 +68,7 @@
                 <b-button
                   variant="outline-primary"
                   class="shadow-sm btn-icon mx-2 mt-1"
-                  @click="handleEditPostRoute"
+                  @click="handleEditPostRoute(`${$route.path}/post`)"
                 >
                   <b-icon icon="pencil-square" />
                 </b-button>
@@ -88,39 +88,25 @@
             </div>
           </b-col>
         </b-row>
-        <b-row class="py-3">
+        <b-row
+          v-for="(block, index) in news.blocks"
+          :key="index"
+          class="py-3"
+        >
           <b-col>
             <div class="d-flex justify-content-between">
               <h3 class="text-primary">
-                川普染疫確診
+                {{ block.blockTitle }}
               </h3>
               <b-link class="text-gray">
-                2020.10.22
+                {{ formatTime(block.blockDateTime) }}
               </b-link>
             </div>
-            <p>
-              川普防疫作為成為這次美國大選核心話題，川普自己從態度輕忽，說新冠肺炎不過就像普通流行性感冒一樣，不需要緊張，到胡亂開藥方，包括宣稱注射漂白水等等離譜的言論，一直到新冠肺炎嚴重性遠超出他的認知，再改口自己防疫得當，否則美國死會更多的人。 紐約時報二日報導，在美國始於新冠肺炎的人突破了20萬人達到207,000人之際，美國總統川普自己也染上了新冠肺炎，檢測確診。
-            </p>
-            <p>
-              『川普在推特上宣布，不只他，包括他的夫人，也都檢測確診染上新冠肺炎，展開自我隔離。』 川普的確診，正讓美國的國家最高權力領導出現不確定性，再加上川普上週在總統大選辯論時，不承諾他如果敗選，會和平交出政權，更讓美國在選舉即將到來之際，政局充滿了變數。
-            </p>
-          </b-col>
-        </b-row>
-        <b-row class="py-3">
-          <b-col>
-            <div class="d-flex justify-content-between">
-              <h3 class="text-primary">
-                川普用藥治療
-              </h3>
-              <b-link class="text-gray">
-                2020.10.22
-              </b-link>
-            </div>
-            <p>
-              10/2 晚一架直昇機停在白宮草坪，川普被目擊戴著藍色口罩走出白宮步上飛機。被白宫接近川普的人士向《紐約時報》透露，川普有些發燒、呼吸不順和咳嗽，才留在白宮一天，醫療團隊就決定將他送到律德軍醫院（Walter Reed）接受治療，而且已經讓他服用一種實驗性的藥物。根據川普的醫生康利（Sean P.Conley）所做的記錄，川普已經服用一種抗體雞尾酒藥物，這是由生技公司Regeneron研發的療法，川普同時也服用維他命D、鋅、褪黑激素、阿斯匹林和一種制酸藥物法莫替丁（英文：famotidine,又名Pepcid）。
-            </p>
-            <p>
-              康利的記錄指出，這種雞尾酒藥物療法的人體實驗還處於初步階段，但初步證明能夠降低人體內的病毒含量。 醫生的紀錄顯示，川普並沒有服用他今年稍早聲稱的「神藥」羥氯奎寧（英文： Hydroxychloroquine）或氯奎寧（英文：Chloroquine）。但這兩項藥後隨後被美國食暨藥物管理局（FDA）撤銷緊急使用權，指出這兩種在治療新冠肺炎上可能無效，還有安全上的疑慮。
+            <p
+              v-for="(content,contentIndex) in block.content.content"
+              :key="contentIndex"
+            >
+              {{ content.content ? content.content[0].text : '' }}
             </p>
           </b-col>
         </b-row>
@@ -153,10 +139,42 @@
 </template>
 
 <script>
+// test id:  5f5113349779a26bd0444b26
 import moment from 'moment'
-
+import { getArticleById } from '@/api/article'
+import { getToken } from '@/utils/auth'
 export default {
   name: 'Article',
+  async asyncData ({ route, store }) {
+    const { ArticleId } = route.params
+    const { data } = await getArticleById(ArticleId)
+    if (data.code === 200) {
+      const responseData = data.data
+      const { title, tags, author, createdAt, blocks, lastUpdatedAt } = responseData
+      blocks.forEach(block => {
+        console.log(block)
+        block.content.content.forEach(content => console.log(content.content))
+      })
+      return {
+        news: {
+          title,
+          tags,
+          lastUpdatedAt,
+          blocks
+        },
+        author,
+        createdAt,
+        blocks,
+        editors: [],
+        editableBlocks: [],
+        isLogin: !!getToken()
+      }
+    } else {
+      return {
+        isLogin: !!getToken()
+      }
+    }
+  },
   data () {
     return {
       tagName: '美國總統大選',
@@ -184,8 +202,11 @@ export default {
     this.time = null
   },
   methods: {
-    handleEditPostRoute () {
-
+    handleEditPostRoute (route) {
+      if (this.isLogin) { this.$router.push(route) } else { this.$bvModal.msgBoxOk('Please Login First') }
+    },
+    formatTime (timeString) {
+      return moment(timeString).format('YYYY-MM-DD HH:mm')
     }
   }
 
---
+++ src/pages/new/article/_ArticleId/post/index.vue
@@ -0,0 +1,384 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col
+        class="py-2 main-editor-area"
+        lg="7"
+        md="12"
+        sm="12"
+        cols="12"
+      >
+        <b-row>
+          <b-col class="d-flex justify-content-between align-items-center">
+            <b-breadcrumb
+              :items="items"
+              class="bg-transparent p-0 m-0"
+            />
+            <div class="editor-hint-step">
+              <span>Help!</span>
+              <span>1. 編輯文章主題區塊</span>
+              <span>2. 撰寫段落標題與內容</span>
+              <span>3. 右側搜尋新聞加入文章</span>
+              <b-button variant="link">
+                <b-icon icon="x" />
+              </b-button>
+            </div>
+          </b-col>
+        </b-row>
+        <b-card
+          no-body
+          class="mt-2 bg-light border-0"
+        >
+          <b-card-body>
+            <b-form>
+              <label
+                class="sr-only"
+                for="post-title"
+              >標題</label>
+              <b-form-input
+                id="post-title"
+                v-model="postTitle"
+                class="mb-0 mr-sm-2 mb-sm-0"
+                placeholder="輸入標題..."
+                required
+              />
+            </b-form>
+            <div class="d-flex justify-content-between py-2">
+              <div class="d-flex justify-content-start">
+                <b-button
+                  v-for="(tag, tagIndex) in postTags"
+                  :key="tagIndex"
+                  variant="outline-primary"
+                  size="sm"
+                  class="tag tag-pill"
+                >
+                  #{{ tag }}
+                  <b-icon
+                    icon="x"
+                    class="ml-1"
+                  />
+                </b-button>
+                <b-button
+                  variant="outline-primary"
+                  class="tag tag-add-btn"
+                >
+                  <b-icon icon="plus" />
+                </b-button>
+              </div>
+              <div class="d-flex justify-content-end align-items-center">
+                <b-form-checkbox
+                  v-model="isAnonymous"
+                  value="true"
+                  unchecked-value="false"
+                >
+                  匿名發文
+                </b-form-checkbox>
+                <span class="mx-2 text-secondary">
+                  編輯者：Shang
+                </span>
+                <b-link class="text-secondary">
+                  +其他10位
+                </b-link>
+              </div>
+            </div>
+          </b-card-body>
+        </b-card>
+        <div class="d-flex justify-content-center mt-3">
+          <div class="block-divider" />
+          <div class="flex-grow-1">
+            <b-button
+              variant="outline-primary"
+              class="add-block-btn"
+              @click="handleAddBlack"
+            >
+              <span>+ 段落</span>
+            </b-button>
+          </div>
+          <div class="block-divider" />
+        </div>
+        <div
+          v-for="block in blocks"
+          :key="block.id"
+        >
+          <b-card
+            no-body
+            class="bg-light border-0 min-vh-50"
+          >
+            <TiptapEditor
+              :content.sync="block.content"
+              :block-title.sync="block.blockTitle"
+              :block-date-time.sync="block.blockDateTime"
+              @onEdit="onEditorEdit"
+            />
+          </b-card>
+          <div class="d-flex justify-content-center mt-3">
+            <div class="block-divider" />
+            <div class="flex-grow-1">
+              <b-button
+                variant="outline-primary"
+                class="add-block-btn"
+                @click="handleAddBlack"
+              >
+                <span>+ 段落</span>
+              </b-button>
+            </div>
+            <div class="block-divider" />
+          </div>
+        </div>
+        <b-row>
+          <b-col class="d-flex justify-content-between">
+            <b-button
+              variant="outline-primary"
+              size="lg"
+              class="px-3"
+              @click="handleSaveArticle"
+            >
+              儲存
+            </b-button>
+            <b-button
+              variant="outline-primary"
+              size="lg"
+              class="px-3"
+              @click="handlePublish"
+            >
+              發布
+            </b-button>
+          </b-col>
+        </b-row>
+      </b-col>
+      <b-col
+        lg="5"
+        cols="12"
+        class="py-4 py-lg-2 news-area"
+      >
+        <NewsPanel @importNews="importNews" />
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import { getArticleById, createArticle, updateArticleById } from '@/api/article'
+import TiptapEditor from '@/components/Post/TiptapEditor'
+import { getToken } from '@/utils/auth'
+import NewsPanel from '@/components/NewsPanel'
+const seperateDateAndTime = (dateTimeString) => {
+  const dateTime = new Date(dateTimeString)
+  return {
+    date: dateTime.toISOString().slice(0, 10),
+    time: dateTime.toLocaleTimeString('en-US', { hour12: false })
+  }
+}
+
+export default {
+  name: 'Post',
+  components: {
+    TiptapEditor,
+    NewsPanel
+  },
+  middleware: 'authenticated',
+  async asyncData ({ route }) {
+    const { ArticleID } = route.params
+    const isNewPost = !(ArticleID || false)
+    if (ArticleID) {
+      const { data } = await getArticleById(ArticleID)
+      if (data.code === 200) {
+        const postData = data.data
+        return {
+          data: postData,
+          articleId: ArticleID,
+          postAuthors: postData.authors,
+          postTitle: postData.title,
+          postTags: postData.tags,
+          postDateTime: postData.createdAt,
+          postDateValue: seperateDateAndTime(postData.createdAt).date,
+          postTimeValue: seperateDateAndTime(postData.createdAt).time,
+          blocks: postData.blocks,
+          isNewPost
+        }
+      }
+    } else {
+      return {
+        isNewPost
+      }
+    }
+  },
+  data () {
+    return {
+      articleId: undefined,
+      currentEditingEditor: null,
+      isNewPost: false,
+      isAnonymous: false,
+      blocks: [],
+      data: {},
+      postAuthors: ['內部測試'],
+      postTitle: '',
+      postDateTime: '',
+      postDateValue: '',
+      postTimeValue: '',
+      postTags: ['美國總統大選', '川普'],
+      items: [
+        {
+          text: '首頁',
+          to: '/new/home'
+        },
+        {
+          text: '編輯新聞',
+          active: true
+        }
+      ]
+    }
+  },
+  methods: {
+    handleAddBlack () {
+      const currentBlockCount = this.blocks.length
+      this.blocks.push({
+        id: currentBlockCount + 1,
+        blockDateTime: new Date().toISOString(),
+        content: null
+      })
+    },
+    handleSaveArticle () {
+      this.data = {
+        title: this.postTitle,
+        tags: this.postTags,
+        authors: this.postAuthors,
+        blocks: this.blocks,
+        createdAt: `${this.postDateValue} ${this.postTimeValue}`,
+        token: this.$store.getters.token
+      }
+      localStorage.setItem('post', this.data)
+    },
+    handlePublish () {
+      // return if the user is not login
+      if (!getToken()) {
+        this.$bvModal.msgBoxOk('Please Login First')
+        return
+      }
+      this.handleSaveArticle()
+      if (this.isNewPost) {
+        createArticle(this.data).then((response) => {
+          console.log(response)
+          if (response.data.code === 200) {
+            this.articleId = response.data.id
+            this.$bvModal.msgBoxOk(response.data.message)
+              .then(() => {
+                this.$router.push({ name: 'article-ArticleID', params: { ArticleID: this.articleId } })
+              })
+          } else {
+            this.$bvModal.msgBoxOk(response.data.message)
+          }
+        }).catch((err) => {
+          console.error(err)
+          this.$bvModal.msgBoxOk(err.data.message)
+        })
+      } else {
+        this.data.id = this.$route.params.ArticleID
+        this.data.token = this.$store.getters.token
+        updateArticleById(this.data).then((response) => {
+          console.log(response)
+          if (response.data.code === 200) {
+            this.$bvModal.msgBoxOk(response.data.message)
+              .then(() => {
+                this.$router.push({ name: 'article-ArticleID', params: { ArticleID: this.articleId } })
+              })
+          } else {
+            this.$bvModal.msgBoxOk(response.data.message)
+          }
+        }).catch((err) => {
+          console.error(err)
+          this.$bvModal.msgBoxOk(err.data.message)
+        })
+      }
+    },
+    importNews (content) {
+      if (this.currentEditingEditor === null) {
+        this.$bvModal.msgBoxOk('請選擇編輯區塊，或是先新增段落後再引入')
+        return
+      }
+      let str = this.currentEditingEditor.getHTML()
+      content.forEach((text) => {
+        str += `<p>${text}</p>`
+      })
+      this.currentEditingEditor.setContent(str, true)
+    },
+    handleClearPost () {
+      this.blocks = []
+      this.data = {}
+      this.postAuthors = []
+      this.postTitle = ''
+      this.postDateValue = ''
+      this.postTimeValue = ''
+      this.postTags = []
+    },
+    onEditorEdit (editor) {
+      this.currentEditingEditor = editor
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/Post/editor.scss";
+
+.block-divider {
+  width: 100%;
+  height: 1rem;
+  border-bottom: 1px solid $primary;
+}
+
+.tag {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 3rem;
+  border: 1px solid $primary;
+  margin: 0.25rem !important;
+
+  &-pill {
+    height: 2rem !important;
+    text-decoration: none;
+    padding: 0.25rem 0.5rem 0.25rem 0.75rem !important;
+  }
+
+  &-add-btn {
+    height: 2rem !important;
+    width: 2rem !important;
+  }
+}
+
+.add-block-btn {
+  border-radius: 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 1rem;
+  width: 5rem;
+  height: 2rem;
+}
+
+.editor-hint-step {
+  display: inline-flex;
+  justify-content: flex-end;
+  align-items: center;
+  background-color: $light;
+  border-radius: 3rem;
+  padding: 0 0 0 1rem;
+
+  span {
+    margin: 0rem 0.5rem;
+  }
+}
+
+.main-editor-area {
+  display:inline-block;
+  height: calc(100vh - 100px);
+  overflow-y: scroll;
+}
+
+.news-area {
+  display:inline-block;
+  height: calc(100vh - 100px);
+  overflow-y: scroll;
+}
+</style>
