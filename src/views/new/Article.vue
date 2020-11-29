04c8472ed8945504138ee22de33df25efe79b095
yslin <s5907610@gmail.com>
2020-11-29 16:27:45 +0800

    feat(views/new/Article): update to newly designed Article page

--- src/views/new/Article.vue
+++ src/views/new/Article.vue
@@ -1,149 +1,157 @@
 <template>
-  <b-container>
+  <b-container fluid="xl">
     <b-row>
-      <b-col
-        cols="auto"
-        class="mr-auto p-3"
-      >
-        <b-button
-          pill
-          variant="outline-secondary"
-          @click="$router.back()"
-        >
-          回上一頁
-        </b-button>
-      </b-col>
-      <b-col
-        cols="auto"
-        class="p-3"
-      >
-        <b-button
-          pill
-          @click="handleEditPostRoute(`${$route.path}/post`)"
-        >
-          編輯新聞
-        </b-button>
-      </b-col>
-    </b-row>
-    <hr>
-    <b-row>
-      <b-col
-        cols="auto"
-        class="mr-auto"
-      >
-        <h1>{{ title }}</h1>
-      </b-col>
-    </b-row>
-    <div class="title-bar">
-      <span class="author">{{ author }}</span>
-    </div>
-    <div class="title-bar">
-      <b-link
-        v-for="tag in tags"
-        :key="tag"
-        class="tag"
-      >
-        #{{ tag }}
-      </b-link>
-      <span class="author">{{ new Date(createdAt).toLocaleString() }}</span>
-    </div>
-
-    <b-img
-      src="https://picsum.photos/300/150/?image=41"
-      fluid-grow
-      alt="Fluid-grow image"
-    />
-    <br>
-    <br>
-    <div
-      v-for="block in blocks"
-      :key="block._id"
-    >
-      <b-row>
-        <b-col
-          cols="auto"
-          class="mr-auto"
-        >
-          <h2>{{ block.blockTitle }}</h2>
-        </b-col>
-        <b-col cols="auto">
-          <div class="btn-actions-pane-right actions-icon-btn">
-            <b-dropdown
-              id="ddown1"
-              variant="transparent"
-              no-caret
-              dropright
-              :disabled="!isLogin"
-            >
-              <template v-slot:button-content>
-                <b-icon
-                  icon="three-dots"
-                  font-scale="1.5"
-                /><span class="sr-only">更多</span>
-              </template>
-              <b-dropdown-item
-                :disabled="isEditting"
-                @click="handleEditBlockStatusChange(block._id, true, block)"
-              >
-                {{ isEditting? '已經為編輯狀態': '編輯此段落' }}
-              </b-dropdown-item>
-              <b-dropdown-item disabled>
-                段落歷史
-              </b-dropdown-item>
-              <b-dropdown-item disabled>
-                編輯次數：3
-              </b-dropdown-item>
-              <b-dropdown-divider />
-              <b-dropdown-item disabled>
-                編輯者
-              </b-dropdown-item>
-            </b-dropdown>
+      <b-col cols="2">
+        <div class="sync-blank-container d-flex flex-column pr-3">
+          <div class="sync-timeline">
+            <div class="sync-timeline-title" />
           </div>
-        </b-col>
-      </b-row>
-      <b-card
-        border-variant="white"
-        no-body
-      >
-        <editor-content
-          class="editor__content"
-          :editor="editors[block._id]"
-        />
-      </b-card>
-      <b-row
-        v-if="getEditable(block._id)"
-        class="p-3 d-block text-right card-footer"
-      >
-        <b-button
-          variant="link"
-          @click="handleEditBlockStatusChange(block._id, false, block)"
-        >
-          取消
-        </b-button>
-        <b-button
-          variant="primary"
-          class="ml-2"
-          @click="handleSubmitBlock(block._id, block)"
-        >
-          儲存並發布
-        </b-button>
-      </b-row>
-      <b-row>
-        <div class="last-update">
-          (最後更新時間：{{ new Date (block.blockDateTime).toLocaleString() }})
+          <div class="sync-timeline">
+            <div class="sync-timeline-time">
+              2020.10.02
+            </div>
+            <div class="sync-timeline-title">
+              <p>川普染疫確診</p>
+            </div>
+          </div>
+          <div class="sync-timeline">
+            <div class="sync-timeline-time">
+              2020.10.02
+            </div>
+            <div class="sync-timeline-title">
+              <p>川普染疫確診</p>
+            </div>
+          </div>
+          <div class="sync-timeline">
+            <div class="sync-timeline-time">
+              2020.10.02
+            </div>
+            <div class="sync-timeline-title">
+              <p>川普染疫確診</p>
+            </div>
+          </div>
+          <div class="sync-timeline">
+            <div class="sync-timeline-time">
+              2020.10.02
+            </div>
+            <div class="sync-timeline-title pl-2 pt-2">
+              <p>川普染疫確診</p>
+            </div>
+          </div>
         </div>
-      </b-row>
-      <hr>
-    </div>
+      </b-col>
+      <b-col cols="8">
+        <b-row>
+          <b-col>
+            <p>[2020 美國總統大選] 這邊不知道是要填什麼資料</p>
+            <h2>
+              {{ title }}
+            </h2>
+            <hr class="my-2">
+            <div class="text-center text-secondary">
+              <b-icon icon="clock" />
+              最後更新時間 {{ formatTime(lastUpdatedAt) }}
+            </div>
+            <div class="d-flex justify-content-between py-3">
+              <div>
+                <b-button
+                  v-for="(tag, tagIndex) in tags"
+                  :key="tagIndex"
+                  variant="outline-primary"
+                  :class="['m-1', 'flex-shrink-0', 'shadow-sm']"
+                  pill
+                >
+                  {{ tag }}
+                </b-button>
+              </div>
+              <div class="d-flex flex-grow-1 justify-content-end">
+                <b-button
+                  variant="outline-primary"
+                  class="shadow-sm btn-icon mx-2 mt-1"
+                  @click="handleEditPostRoute(`${$route.path}/post`)"
+                >
+                  <b-icon icon="pencil-square" />
+                </b-button>
+                <b-button
+                  variant="outline-primary"
+                  class="shadow-sm btn-icon mx-2 mt-1"
+                >
+                  <b-icon icon="clock-history" />
+                </b-button>
+                <b-button
+                  variant="outline-primary"
+                  class="shadow-sm btn-icon ml-2 mt-1"
+                >
+                  <b-icon icon="bookmark" />
+                </b-button>
+              </div>
+            </div>
+          </b-col>
+        </b-row>
+        <b-row
+          v-for="(block, index) in blocks"
+          :key="index"
+          class="py-3"
+        >
+          <b-col>
+            <div class="d-flex justify-content-between">
+              <h3 class="text-primary">
+                {{ block.blockTitle }}
+              </h3>
+              <b-link class="text-gray">
+                {{ formatTime(block.blockDateTime) }}
+              </b-link>
+            </div>
+            <b-card
+              border-variant="white"
+              no-body
+            >
+              <editor-content
+                class="editor__content"
+                :editor="editors[block._id]"
+              />
+            </b-card>
+          </b-col>
+        </b-row>
+      </b-col>
+      <b-col cols="2">
+        <div class="sync-blank-container" />
+        <div class="sync-time-container py-2">
+          <p>{{ time.year() }}</p>
+          <span
+            id="month"
+            class="mb-2"
+          >{{ time.month() + 1 }} 月</span>
+          <span
+            id="date"
+            class="mb-2"
+          >{{ time.date() }} 日</span>
+          <p class="mt-4 mb-0">
+            {{ time.format('hh:mm a') }}
+          </p>
+        </div>
+        <div class="sync-authors-container py-2">
+          <p class="mb-1">
+            編輯者：
+          </p>
+          <b-link
+            v-for="(author, index) in authors"
+            :key="index"
+          >{{ formatAuthor(author, index === authors.length-1) }}</b-link>
+          <!-- <b-link>吳先生</b-link>, <b-link>吳先生</b-link>, <b-link>吳先生</b-link>, <b-link>吳先生</b-link> -->
+        </div>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
 <script>
-import { getArticleById, updateArticleById } from '@/api/article'
+// test id:  5f5113349779a26bd0444b26
+import moment from 'moment'
+import { getArticleById } from '@/api/article'
 import { Editor, EditorContent } from 'tiptap'
 import { Heading, Bold, Italic, Strike, Underline, BulletList, ListItem, Placeholder } from 'tiptap-extensions'
 import Link from '@/components/new/tiptap-extensions/Link'
-
 export default {
   name: 'Article',
   components: {
@@ -151,12 +159,18 @@ export default {
   },
   data() {
     return {
-      data: null,
+      tagName: '美國總統大選',
+      order: 'time',
+      category: [],
       title: '',
+      viewCount: 32,
       tags: [],
-      author: '',
-      createdAt: '',
       blocks: [],
+      authors: [],
+      createdAt: '',
+      lastUpdatedAt: '',
+      timeId: null,
+      time: moment(),
       editors: [],
       editableBlocks: [],
       isEditting: false,
@@ -165,19 +179,33 @@ export default {
       isLogin: false
     }
   },
-  beforeDestroy() {
-    this.editors.forEach(editor => editor.destroy())
+  computed: {
+    getYear() {
+      return this.time.getFullYear()
+    },
+    getMonth() {
+      return this.time.getMonth()
+    }
   },
   created() {
+    this.timeId = setInterval(() => {
+      this.time = moment()
+    }, 1000)
     const articleId = this.$route.params.ArticleID
     getArticleById(articleId).then(response => {
       if (response.data.code === 200) {
-        const data = this.data = response.data.data
-        this.title = data.title
-        this.tags = data.tags
-        this.author = data.author
-        this.createdAt = data.createdAt
-        this.blocks = data.blocks
+        const { title, authors, tags, createdAt, blocks, lastUpdatedAt, category, editedCount, editingCount, isPopular } = response.data.data
+        console.log(response.data.data)
+        this.title = title
+        this.authors = authors
+        this.tags = tags
+        this.createdAt = createdAt
+        this.lastUpdatedAt = lastUpdatedAt
+        this.blocks = blocks
+        this.category = category
+        this.editedCount = editedCount
+        this.editingCount = editingCount
+        this.isPopular = isPopular
         this.blocks.forEach(block => {
           this.editors[block._id] = this.createEditor(block.content)
           this.editableBlocks[block._id] = false
@@ -189,7 +217,20 @@ export default {
     // check if user logged in
     this.isLogin = !!this.$store.getters.token
   },
+  beforeDestroy() {
+    clearInterval(this.timeId)
+    this.time = null
+  },
   methods: {
+    handleEditPostRoute(route) {
+      if (this.isLogin) { this.$router.push(route) } else { this.$bvModal.msgBoxOk('Please Login First') }
+    },
+    formatTime(timeString) {
+      return moment(timeString).format('YYYY-MM-DD HH:mm')
+    },
+    formatAuthor(author, isTheLastAuthor) {
+      return isTheLastAuthor ? author : author + ', '
+    },
     createEditor(initializedContent) {
       const editor = new Editor({
         autoFocus: true,
@@ -222,96 +263,67 @@ export default {
         editable: false
       })
       return editor
-    },
-    getEditable(blockId) {
-      return this.editors[blockId] !== undefined ? this.editableBlocks[blockId] : false
-    },
-    handleEditBlockStatusChange(blockId, value, block) {
-      console.log(value)
-      this.editors[blockId].setOptions({
-        editable: value || false
-      })
-      this.$set(this.editableBlocks, blockId, value)
-      this.isEditting = value
-      if (value) {
-        this.backupBlock = Object.assign(block)
-      } else if (value === false) {
-        block = Object.assign(this.backupBlock)
-        this.editors[blockId].setContent(block.content)
-      }
-    },
-    handleSubmitBlock(blockId, block) {
-      // TODO: 或許需要判定編輯哪個Block，只傳更改的Block到後端
-      this.handleEditBlockStatusChange(blockId)
-      this.data.id = this.$route.params.ArticleID
-      this.data.token = this.$store.getters.token
-      // 取代更新Block
-      block.content = this.editors[blockId].getJSON()
-      updateArticleById(this.data).then((response) => {
-        console.log(response)
-        if (response.data.code === 200) {
-          this.backupBlock = null
-          this.$bvModal.msgBoxOk(response.data.message)
-            .then(() => {
-              const data = this.data = response.data.data
-              this.title = data.title
-              this.tags = data.tags
-              this.author = data.author
-              this.createdAt = data.createdAt
-              this.blocks = data.blocks
-              this.blocks.forEach((block) => {
-                if (this.editors[block.id] !== undefined) {
-                  this.editors[block.id].setContent(block.content)
-                } else { this.editors[block.id] = this.createEditor(block.content) }
-                this.editableBlocks[block.id] = false
-              })
-            })
-        } else {
-          this.$bvModal.msgBoxOk(response.data.message)
-        }
-      }).catch((err) => {
-        console.error(err)
-        this.$bvModal.msgBoxOk(err.data.message)
-      })
-    },
-    handleEditPostRoute(route) {
-      if (this.isLogin) { this.$router.push(route) } else { this.$bvModal.msgBoxOk('Please Login First') }
     }
   }
+
 }
 </script>
 
 <style scoped lang="scss">
-@import "@/assets/Article/main";
-  .reference {
-    color: gray;
-    text-decoration-line: underline;
+@import '@/assets/scss/news.scss';
+
+p {
+  letter-spacing: 0.25rem;
+}
+
+.btn-icon {
+  height: 3rem !important;
+  width: 3rem !important;
+  padding-top: 0.25rem;
+  border-radius: 0.75rem;
+}
+
+.sync-blank-container {
+  padding-top: 9rem;
+}
+
+.sync-timeline {
+  .sync-timeline-time {
+    width: 8rem;
+    border: 1px solid $secondary;
+    color: $secondary;
+    text-align: center;
   }
-  ul {
-    padding-inline-start: 0;
-    list-style: none;
+  .sync-timeline-title {
+    flex-grow: 1;
+    margin-left: 1rem;
+    padding: 0.5rem 0rem 0rem 0.5rem;
+    border-left: 1px solid $black;
+    min-width: 100%;
+    height: 100%;
+    min-height: 3rem;
   }
-  p {
-    padding: 2rem 0;
-  }
-  hr {
-    padding-bottom: 1rem;
-  }
-  .last-update {
-    margin: auto;
-    color: gray;
-  }
-  .title-bar {
-    display: flex;
-  }
-  .author {
-    display: inline-block;
-    margin-left: auto !important;
-    color: gray;
-  }
-  .tag {
-    display: inline-block;
-    padding: 0.5rem;
-    color: gray;
-  }
+  display: flex;
+  flex-direction: column;
+  min-width: 100%;
+  min-height: 3rem;
+}
+
+.sync-time-container {
+  padding-top: 9rem;
+  border-top: 1px solid $black;
+  border-bottom: 1px solid $black;
+}
+
+.sync-authors-container {
+  border-bottom: 1px solid $black;
+}
+
+#month {
+  font-size: 2rem;
+}
+
+#date {
+  font-size: 1.5rem;
+}
 </style>
