74055a9f330ecf45b2d32792aab705be1ae7af04
newb1er <taiwanmichael429@gmail.com>
2021-08-28 16:15:30 +0800

    ✨ add PreferenceItem component

---
+++ src/components/Profile/PreferenceItem.vue
@@ -0,0 +1,68 @@
+<template>
+  <b-container class="no-gutters pl-0 pr-0 pb-3 mt-2">
+    <b-form-checkbox
+      v-model="preference.status"
+      switch
+      class="switch float-right"
+    />
+    <div>
+      {{ preference.title }} <br>
+      {{ preference.description }}
+    </div>
+  </b-container>
+</template>
+
+<script>
+export default {
+  props: {
+    preference: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+/*
+  custom color variables
+  TODO: merge into custom.scss
+*/
+$Neture-N-3: #ededf0;
+
+// FIXME: outline appears when toggling
+.custom-switch {
+  $width: 3rem;
+  $height: calc(#{$width} / 2);
+
+  width: $width;
+  height: $height;
+
+  & > input {
+    & ~ label::before {
+      border-color: $Neture-N-3 !important;
+      background-color: $Neture-N-3 !important;
+      width: $width !important;
+      height: $height !important;
+      border-radius: $width !important;
+    }
+
+    & ~ label::after {
+      background-color: white !important;
+      width: calc(#{$height} - 4px) !important;
+      height: calc(#{$height} - 4px) !important;
+      border-radius: 50% !important;
+    }
+
+    &:checked {
+      & ~ label::before {
+        border-color: $blue !important;
+        background-color: $blue !important;
+      }
+      & ~ label::after {
+        transform: translateX($height) !important;
+      }
+    }
+  }
+}
+</style>
