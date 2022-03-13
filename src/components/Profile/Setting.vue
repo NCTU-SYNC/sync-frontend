d58e67bf95ebcf20838afc54ccc8a41671393dd0
Michael <32424677+newb1er@users.noreply.github.com>
2022-03-14 01:46:12 +0800

    [SYNC-166][FE] Pass user's settings to backend (#49)
    * [SYNC-169] ✨ update display name using firebase authentication
    
    * [SYNC-169][FE] ✨ bind preferences settings with buttons
    
    * [SYNC-169][FE] ✨ new APIs
    
    * [SYNC-169][FE] ✨ fetch preferences when logging in
    
    * [SYNC-171][FE] ✨ new '/profile/updateNameModTime' API
    
    * [SYNC-171][FE] ✨ Store 'nameModTime' when logging in
    
    * [SYNC-171][FE] No name modification if it is modified twice in a month
    
    * [SYNC-171][FE] ✨ update style of button

--- src/components/Profile/Setting.vue
+++ src/components/Profile/Setting.vue
@@ -6,53 +6,65 @@
     </div>
     <div class="section">
       <h4>顯示名稱</h4>
-      <div class="text-box">
-        <span class="m-0 flex-grow-1">{{ displayName }}</span>
-        <span class="text-box-divider" />
-        <b-button
-          id="rename-btn"
-          v-b-modal.rename-modal
-          variant="white"
-          size="lg"
-          class="m-0 font-size-1"
-        >更改</b-button>
-        <b-modal
-          id="rename-modal"
-          centered
-          title="更改顯示名稱"
-          title-class="font-weight-bold"
-          header-class="rename-modal-header border-0"
-          footer-class="border-0"
-        >
-          <b-container>
-            <b-form>
-              <b-form-group>
-                <template #description>
-                  <p class="mt-3">
-                    顯示名稱在一個月內僅可以更改兩次。<br>
-                    且更改後，將會影響你過去所編輯過文章的名稱。
-                  </p>
-                </template>
-                <b-form-input id="input-username" size="lg" required />
-              </b-form-group>
-            </b-form>
-          </b-container>
-          <template #modal-footer="{ ok, cancel }">
-            <b-button
-              class="rename-modal-btn"
-              variant="white"
-              type="button"
-              @click="cancel()"
-            >取消</b-button>
-            <b-button
-              id="btn-ok"
-              class="rename-modal-btn"
-              variant="white"
-              type="submit"
-              @click="ok()"
-            >儲存</b-button>
-          </template>
-        </b-modal>
+      <div class="d-flex">
+        <div class="text-box">
+          <span class="m-0 flex-grow-1">{{ displayName }}</span>
+        </div>
+        <div class="ml-3 rename-btn">
+          <b-button
+            id="rename-btn"
+            v-b-modal.rename-modal
+            variant="white"
+            size="lg"
+            class="m-0 font-size-1"
+            :disabled="!nameChangable"
+          >更改</b-button>
+          <b-modal
+            id="rename-modal"
+            centered
+            title="更改顯示名稱"
+            title-class="font-weight-bold"
+            header-class="rename-modal-header border-0"
+            footer-class="border-0"
+          >
+            <b-container>
+              <b-form>
+                <b-form-group>
+                  <template #description>
+                    <p v-if="!nameChangable">
+                      您已在這一個月內更改兩次顯示名稱
+                    </p>
+                    <p class="mt-3">
+                      顯示名稱在一個月內僅可以更改兩次。<br>
+                      且更改後，將會影響你過去所編輯過文章的名稱。
+                    </p>
+                  </template>
+                  <b-form-input
+                    id="input-username"
+                    v-model="inputName"
+                    size="lg"
+                    required
+                  />
+                </b-form-group>
+              </b-form>
+            </b-container>
+            <template #modal-footer="{ cancel }">
+              <b-button
+                class="rename-modal-btn"
+                variant="white"
+                type="button"
+                @click="cancel()"
+              >取消</b-button>
+              <b-button
+                id="btn-ok"
+                class="rename-modal-btn"
+                variant="white"
+                type="submit"
+                @click="confirm"
+              >儲存</b-button>
+            </template>
+          </b-modal>
+        </div>
       </div>
     </div>
     <div class="section pb-3">
@@ -64,10 +76,11 @@
     <div class="section pt-5">
       <h4>偏好設定</h4>
       <PreferenceItem
-        v-for="(preference, preferenceIndex) in mockPreference"
+        v-for="(preference, preferenceIndex) in preferences"
         :key="preferenceIndex"
-        class="setting-pref"
         :preference="preference"
+        class="setting-pref"
+        @change="handleOnChange"
       />
     </div>
   </b-container>
@@ -75,14 +88,20 @@
 
 <script>
 import PreferenceItem from './PreferenceItem.vue'
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
+import moment from 'moment'
 
 /* TODO: replace with API */
 class Preference {
-  constructor(title, description, status) {
+  constructor(title, description, name, status) {
     this.title = title
     this.description = description
+    this.name = name
     this.status = status
+  }
+
+  value() {
+    return [this.name, this.status]
   }
 }
 
@@ -90,27 +109,56 @@ export default {
   components: {
     PreferenceItem
   },
-  computed: {
-    ...mapGetters(['displayName', 'photoURL']),
-    ...mapGetters({ email: 'user/email' }),
-    mockPreference() {
-      return [
+  data() {
+    return {
+      inputName: '',
+      preferences: [
         new Preference(
           '匿名發文',
           '開啟後您的新增段落與文章預設作者都將匿名',
-          false
+          'isAnonymous',
+          this.$store.state.user.preferences.isAnonymous
         ),
         new Preference(
           '編輯文章更新通知',
           '開啟後您曾編輯過的文章有任何更新都將通知您',
-          false
+          'editedNotification',
+          this.$store.state.user.preferences.editedNotification
         ),
         new Preference(
           '收藏文章更新通知',
           '開啟後您的收藏文章有任何更新都將通知您',
-          false
+          'subscribedNotification',
+          this.$store.state.user.preferences.subscribedNotification
         )
       ]
+    }
+  },
+  computed: {
+    ...mapGetters(['displayName', 'photoURL']),
+    ...mapGetters({ email: 'user/email', nameModTime: 'user/nameModTime' }),
+    nameChangable() {
+      if (this.nameModTime.length < 2) return true
+      const monthAgo = moment().subtract(1, 'months')
+      return this.nameModTime.some((time) => moment(time).isBefore(monthAgo))
+    }
+  },
+  methods: {
+    ...mapActions({
+      updateDisplayName: 'user/updateDisplayName',
+      updatePreferences: 'user/updatePreferences'
+    }),
+    confirm() {
+      if (this.nameChangable) {
+        this.updateDisplayName(this.inputName)
+      }
+      this.$bvModal.hide('rename-modal')
+    },
+    handleOnChange() {
+      const pref = Object.fromEntries(
+        this.preferences.map((pref) => pref.value())
+      )
+      this.updatePreferences(pref)
     }
   }
 }
@@ -131,12 +179,22 @@ export default {
   }
 }
 
+.rename-btn {
+  width: 4rem;
+
+  button {
+    background: $blue;
+    color: $white !important;
+    border: 0;
+  }
+}
+
 .text-box {
   display: flex;
+  flex: 1;
   border: 1px solid $gray-400;
   border-radius: 5px;
-  width: 100%;
-  height: 3rem;
+  height: 2.5rem;
   align-items: center;
   justify-content: space-between;
   padding-left: 1rem;
--- src/store/user.js
+++ src/store/user.js
@@ -1,12 +1,23 @@
-import { login } from '@/api/user'
+import { login, updateNameModTime, updateProfilePref } from '@/api/user'
 import { setUserInfo, getUserInfo } from '@/utils/auth'
 import FirebaseAuth from '@/utils/firebase.js'
+
+/* ! TODO: replace with vuex action [SYNC-154] */
+import FirebaseAuthInstance from '@/utils/firebase'
 
 const getDefaultState = () => {
   return {
     isInitialized: false,
     name: '',
     displayName: getUserInfo() ? getUserInfo().displayName : '',
+    preferences: getUserInfo()
+      ? getUserInfo().preferences
+      : {
+        isAnonymous: false,
+        editedNotification: false,
+        subscribedNotification: false
+      },
+    nameModTime: [],
     email: getUserInfo() ? getUserInfo().email : '',
     uid: getUserInfo() ? getUserInfo().uid : '',
     gender: '',
@@ -22,6 +33,8 @@ const state = getDefaultState()
 const getters = {
   createAt: (state) => state.createAt,
   email: (state) => state.email,
+  preferences: (state) => state.preferences,
+  nameModTime: (state) => state.nameModTime
   expirationTime: (state) => state.expirationTime
 }
 
@@ -40,6 +53,18 @@ const mutations = {
   },
   SET_NOTIFICATIONS(state, notifications) {
     state.notifications = notifications
+  },
+  SET_DISPALY_NAME(state, displayName) {
+    state.displayName = displayName
+    const data = getUserInfo()
+    data.displayName = displayName
+    setUserInfo(data)
+  },
+  SET_PREFERENCES(state, preferences) {
+    state.preferences = preferences
+  },
+  SET_NAME_MOD_TIME(state, nameModTime) {
+    state.nameModTime = nameModTime
   }
 }
 
@@ -67,6 +92,43 @@ const actions = {
   },
   removeUser({ commit }) {
     commit('RESET_USER')
+  },
+  updateDisplayName({ commit }, displayName) {
+    commit('SET_DISPALY_NAME', displayName)
+    FirebaseAuthInstance.instance.currentUser
+      .updateProfile({
+        displayName: displayName
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+    FirebaseAuthInstance.instance.currentUser
+      .getIdToken()
+      .then((token) => {
+        updateNameModTime({
+          token: token
+        }).then((res) => commit('SET_NAME_MOD_TIME', res.data.data))
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  },
+  setPreferences({ commit }, preferences) {
+    commit('SET_PREFERENCES', preferences)
+  },
+  updatePreferences({ commit }, preferences) {
+    commit('SET_PREFERENCES', preferences)
+    FirebaseAuthInstance.instance.currentUser
+      .getIdToken()
+      .then((token) => {
+        updateProfilePref({
+          token: token,
+          payload: { preferences: preferences }
+        })
+      })
+      .catch((error) => {
+        console.error(error)
+      })
   }
 }
 
