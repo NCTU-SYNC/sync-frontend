ddeac59b597e38dd2c0fe8e0757d4b46e910765a
newb1er <32424677+newb1er@users.noreply.github.com>
2022-03-21 20:03:09 +0800

    [SYNC-48][FE] 💡 add comments

--- src/utils/citationManager.js
+++ src/utils/citationManager.js
@@ -1,14 +1,24 @@
 import store from '@/store/index'
 
 class CitationNode {
-  constructor(node, editorId) {
+  /**
+   * constructor of CitationNode
+   * @param {Node} node
+   */
+  constructor(node) {
     this.node = node
   }
 
+  /**
+   * get id of the editor that owns this node
+   */
   get editorId() {
     return this.node.$props.editor.view.dom.id
   }
 
+  /**
+   * get pos of the node in the editor
+   */
   get pos() {
     return this.node.getPos()
   }
@@ -34,7 +44,7 @@ class CitationInfo {
   /**
    * connect node with citation
    *
-   * @param {CitationNode} node
+   * @param {CitationNode} node node to be connected
    */
   addNode(node) {
     const editorId = store.state.post.currentEditingEditor.view.dom.id
@@ -44,6 +54,11 @@ class CitationInfo {
     this.sortNodesArray()
   }
 
+  /**
+   *
+   * @param {CitationNode} node node to be removed
+   * @returns Boolean true if list of node is empty after removing
+   */
   async removeNode(node) {
     this.nodes.splice(this.nodes.indexOf(node), 1)
 
@@ -51,12 +66,18 @@ class CitationInfo {
     return false
   }
 
+  /**
+   * delete all node in next tick
+   */
   removeAllNode() {
     for (const node of this.nodes) {
       node.node.$nextTick(() => node.node.deleteNode())
     }
   }
 
+  /**
+   * sort list of nodes from top to bottom
+   */
   async sortNodesArray() {
     const blocks = store.state.post.blocks.map((b) => b.id)
 
@@ -77,6 +98,10 @@ class CitationInfo {
     this.nodes = Object.values(bucket).flatMap((b) => b)
   }
 
+  /**
+   * get last node of this citation
+   * @returns {CitationNode}
+   */
   get lastNode() {
     return this.nodes[this.nodes.length - 1]
   }
@@ -98,12 +123,21 @@ class CitationInfo {
     this.info = Object.assign({}, this.info, { title, url })
   }
 
+  /**
+   * update index of this citation
+   * @param {Number} index new index of node to be updated
+   */
   updateIndex(index) {
     this.info = Object.assign({}, this.info, { index })
   }
 }
 
 class CitationOpError extends Error {
+  /**
+   * Custom error for operation on citation
+   * @param {String} msg message of error
+   * @param  {...any} params other parameters
+   */
   constructor(msg, ...params) {
     super(msg, ...params)
 
@@ -120,6 +154,12 @@ const CitationInfoHandler = {
   get: (target, prop) => {
     return target[prop]
   },
+  /**
+   * avoid setting properties directly on the target
+   * @param {CitatoinInfo} target
+   * @param {String} prop key of target
+   * @param {Object} value new value of target[key]
+   */
   set: (target, prop, value) => {
     if (prop !== 'info') {
       throw new CitationOpError('Improper operation: No such property')
@@ -132,11 +172,22 @@ const CitationInfoHandler = {
 }
 
 class CitationManager {
+  /**
+   * constructor of CitationManager
+   */
   constructor() {
     this.citationList = []
     this.citationMap = new Map()
   }
 
+  /**
+   * connect node with citation.
+   * if citation not existed, create new one.
+   * @param {String} title title of citation
+   * @param {String} url url of citation
+   * @param {Node} node node to be connected
+   * @returns {Proxy} proxy of citationInfo
+   */
   registerNode(title, url, node) {
     const key = `${title}-${url}`
 
@@ -155,6 +206,11 @@ class CitationManager {
     return new Proxy(citationInfo, CitationInfoHandler)
   }
 
+  /**
+   * disconnect node from citation
+   * @param {Proxy} citation proxy of citationInfo
+   * @param {Node} node citation node in tiptap editor
+   */
   unregisterNode(citation, node) {
     const { title, url } = citation.info
     const key = `${title}-${url}`
@@ -175,6 +231,12 @@ class CitationManager {
     })
   }
 
+  /**
+   * update citation info
+   * @param {Proxy} citation proxy of citationInfo
+   * @param {String} title new title
+   * @param {String} url new url
+   */
   updateCitation(citation, title, url) {
     const { title: oldTitle, url: oldUrl } = citation.info
     const key = `${oldTitle}-${oldUrl}`
@@ -193,6 +255,10 @@ class CitationManager {
     this.citationMap.set(newKey, citationInfo)
   }
 
+  /**
+   * remove citation including all nodes in the post
+   * @param {Proxy} citation proxy of citationInfo
+   */
   removeCitation(citation) {
     const { title, url } = citation.info
     const key = `${title}-${url}`
@@ -206,12 +272,18 @@ class CitationManager {
     citationInfo.removeAllNode()
   }
 
+  /**
+   * @returns {Array<Proxy>} list of proxy of citationInfo
+   */
   getList() {
     return this.citationList.map(
       (citationInfo) => new Proxy(citationInfo, CitationInfoHandler)
     )
   }
 
+  /**
+   * sort citation list from top to bottom by last node of each citation
+   */
   async sortList() {
     const blocks = store.state.post.blocks.map((b) => b.id)
 
@@ -234,6 +306,9 @@ class CitationManager {
     await this.updateCitationIndex()
   }
 
+  /**
+   * update index of each citation (usually after sorting)
+   */
   async updateCitationIndex() {
     this.citationList.forEach((citation, index) => {
       citation.updateIndex(index)
