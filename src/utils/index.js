677b18349977af1033d968bb3a265de3c9e72db5
newb1er <32424677+newb1er@users.noreply.github.com>
2022-03-26 01:07:14 +0800

    [SYNC-183][FE][Post] feat(LinkModal.vue): Implement link validation before submission

--- src/utils/index.js
+++ src/utils/index.js
@@ -31,9 +31,47 @@ function getArticleText(object, result, limit) {
   }
 }
 
+// returns first image in aricle, if no images exist, return null
+function getArticleFirstImage(blocks) {
+  const res = { imgLinks: [] }
+  getArticleImages(blocks, res, 1)
+  return res.imgLinks.length === 0 ? null : res.imgLinks[0]
+}
+
+// returns first "limit" numbers of image from article
+function getArticleImages(blocks, result, limit = 10) {
+  if (blocks == null) return
+  if (Object.prototype.hasOwnProperty.call(blocks, 'type') && blocks.type === 'image') {
+    result.imgLinks.push(blocks.attrs.src)
+    if (result.imgLinks.length >= limit) {
+      return
+    }
+  }
+  for (let i = 0; i < Object.keys(blocks).length; i++) {
+    if (typeof blocks[Object.keys(blocks)[i]] === 'object') {
+      getArticleImages(blocks[Object.keys(blocks)[i]], result, limit)
+      if (result.imgLinks.length >= limit) {
+        return
+      }
+    }
+  }
+}
+
+// url validation
+function isValidUrl(url) {
+  try {
+    new URL(url)
+  } catch (e) {
+    return false
+  }
+  return true
+}
+
 class Utils {
   static getRandomString = getRandomString
   static getArticleContent = getArticleContent
+  static getArticleFirstImage = getArticleFirstImage
+  static isValidUrl = isValidUrl
 }
 
 export { Utils }
