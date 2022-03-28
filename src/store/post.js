1921620409bca2bb9e7e0b68e88558f2d325d3a2
newb1er <32424677+newb1er@users.noreply.github.com>
2022-03-28 18:34:41 +0800

    [SYNC-48][FE] 🩹 CitationManager should only be instantiated once

--- src/store/post.js
+++ src/store/post.js
@@ -1,6 +1,9 @@
 import CitationManager from '../components/Editor/CitationManager'
 
+const citationManager = new CitationManager()
+
 const getDefaultState = () => {
+  citationManager.reset()
   return {
     articleId: undefined,
     isNewPost: false,
@@ -10,7 +13,7 @@ const getDefaultState = () => {
     postTitle: '',
     postTags: [],
     blocks: [],
-    citation: new CitationManager(),
+    citation: citationManager,
     categorySelected: '',
     currentEditingEditor: null,
     showAddPointsAlert: false,
