00464544b79a5888a7c09a8d183e440b5044b0fa
newb1er <32424677+newb1er@users.noreply.github.com>
2022-06-15 02:09:11 +0800

    [FE][SYNC-205] fix: firebase should not be access while unit testing

--- tests/jest-setup.js
+++ tests/jest-setup.js
@@ -3,7 +3,6 @@ import '@testing-library/jest-dom'
 import 'intersection-observer'
 import Vue from 'vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import FirebaseAuth from '@/utils/firebase.js'
 import SvgIcon from '@/components/SvgIcon'
 import '@/assets/scss/main.scss'
 
@@ -14,4 +13,3 @@ Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 Vue.config.productionTip = false
-Vue.prototype.$firebaseAuth = FirebaseAuth
