884e4bdbe00169307002a437edb85050d699fcad
newb1er <32424677+newb1er@users.noreply.github.com>
2022-06-14 22:37:17 +0800

    chore: install vue plugins before tests

--- tests/jest-setup.js
+++ tests/jest-setup.js
@@ -1,1 +1,17 @@
 import '@testing-library/jest-dom'
+
+import 'intersection-observer'
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import FirebaseAuth from '@/utils/firebase.js'
+import SvgIcon from '@/components/SvgIcon'
+import '@/assets/scss/main.scss'
+
+Vue.component('icon', SvgIcon)
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+Vue.config.productionTip = false
+Vue.prototype.$firebaseAuth = FirebaseAuth
