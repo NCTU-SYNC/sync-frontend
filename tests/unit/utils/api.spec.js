9dd2fef7352d3b66f3a831086333726651c737a6
newb1er <32424677+newb1er@users.noreply.github.com>
2022-06-30 22:40:19 +0800

    [FE][SYNC-204] test: update utils/api

--- tests/unit/utils/api.spec.js
+++ tests/unit/utils/api.spec.js
@@ -1,29 +1,8 @@
-import { api, getBaseURL } from '@/utils/api'
-import firebaseAuth from '@/utils/firebase'
-import nock from 'nock'
+import { api } from '@/utils/api'
 
 describe('api', () => {
-  it('POST: data should contains "token"', async() => {
-    const getTokenMock = jest.spyOn(firebaseAuth, 'token', 'get')
-    getTokenMock.mockImplementation(() => 'token')
-
-    const header = {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true
-    }
-
-    const scope = nock(getBaseURL())
-      .defaultReplyHeaders(header)
-      .post('/', (body) => body.token)
-      .reply(200)
-
-    const res = await api.post('/')
-
-    expect(getTokenMock).toBeCalled()
-
-    expect(res.status).toEqual(200)
-
-    scope.done()
+  it('api should contains baseURL', () => {
+    const want = process.env.VUE_APP_BASE_URL + process.env.VUE_APP_API_URL
+    expect(api.defaults.baseURL).toBe(want)
   })
 })
