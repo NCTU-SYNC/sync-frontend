65bdd6f3d585fc63dabacf02b88ade1adef69ae9
newb1er <32424677+newb1er@users.noreply.github.com>
2022-06-30 22:38:53 +0800

    [FE][SYNC-204] fix: rm request interceptor

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,15 +1,7 @@
 import axios from 'axios'
-import _ from 'lodash'
-
-import firebaseAuth from '@/utils/firebase'
-import store from '@/store'
 
 export function getBaseURL() {
   return process.env.VUE_APP_BASE_URL + process.env.VUE_APP_API_URL
-}
-
-export function getToken() {
-  return firebaseAuth.token
 }
 
 const api = axios.create({
@@ -17,22 +9,6 @@ const api = axios.create({
   withCredentials: true,
   timeout: 5000
 })
-
-api.interceptors.request.use(
-  (config) => {
-    if (config.method !== 'post' && config.method !== 'put') return config
-
-    if (!_.has(config, 'data')) {
-      config.data = {}
-    }
-    config.data.token = getToken()
-
-    return config
-  },
-  (err) => {
-    return Promise.reject(err)
-  }
-)
 
 api.interceptors.response.use(
   /**
