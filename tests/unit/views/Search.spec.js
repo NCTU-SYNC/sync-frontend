ccf6536ece3d4727a97890562dc3eaf72598a477
newb1er <32424677+newb1er@users.noreply.github.com>
2022-07-01 22:36:08 +0800

    [FE][SYNC-204] test: views/Search

---
+++ tests/unit/views/Search.spec.js
@@ -0,0 +1,83 @@
+import { render, waitFor, fireEvent } from '@testing-library/vue'
+import Search from '@/views/new/Search.vue'
+import articleAPI from '@/api/article'
+
+jest.mock('@/store')
+jest.mock('@/api/article')
+
+const mockRoute = {
+  query: {
+    q: 'test'
+  }
+}
+const mockRouter = {
+  push: jest.fn()
+}
+
+describe('Time Button', () => {
+  describe('init style', () => {
+    it.concurrent.each([
+      { btnName: '不限', queryTbs: 'qdr:a' },
+      { btnName: '上個月', queryTbs: 'qdr:m' },
+      { btnName: '上禮拜', queryTbs: 'qdr:w' }
+    ])(
+      '"$btnName" is bold when time query is "$queryTbs"',
+      async({ btnName, queryTbs }) => {
+        const searchAPI = jest.spyOn(articleAPI, 'search')
+        searchAPI.mockImplementation(() => Promise.resolve({
+          data: { type: 'success', data: [] }
+        }))
+
+        mockRoute.query.tbs = queryTbs
+
+        const { getByText } = render(Search, {
+          mocks: {
+            $route: mockRoute,
+            $router: mockRouter
+          }
+        })
+
+        await waitFor(() => {
+          expect(getByText(btnName)).toHaveClass('active')
+        })
+      })
+  })
+
+  describe('click', () => {
+    it.concurrent.each([
+      { btnName: '不限', queryTbs: 'qdr:a' },
+      { btnName: '上個月', queryTbs: 'qdr:m' },
+      { btnName: '上禮拜', queryTbs: 'qdr:w' }
+    ])(
+      'time query is "$queryTbs" when "$btnName" is clicked',
+      async({ btnName, queryTbs }) => {
+        const searchAPI = jest.spyOn(articleAPI, 'search')
+        searchAPI.mockImplementation(() => Promise.resolve({
+          data: { type: 'success', data: [] }
+        }))
+
+        mockRoute.query.tbs = 'qdr:d'
+
+        const { getByText } = render(Search, {
+          mocks: {
+            $route: mockRoute,
+            $router: mockRouter
+          }
+        })
+
+        const btn = await waitFor(() => getByText(btnName))
+        await fireEvent.click(btn)
+
+        expect(mockRouter.push).toHaveBeenCalledWith({
+          path: 'search',
+          query: {
+            q: 'test',
+            tbs: queryTbs
+          }
+        })
+
+        // btn should be active after being clicked
+        expect(btn).toHaveClass('active')
+      })
+  })
+})
