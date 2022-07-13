c0e401855bfc68054bb42c1183911b243561d1cf
David Chiu <david20571015@gmail.com>
2022-07-14 00:07:06 +0800

    [SYNC-210][FE] update: login UI

--- src/views/new/Login2.vue
+++ src/views/new/Login2.vue
@@ -1,38 +1,82 @@
 <template>
-  <b-modal v-model="modalShow" hide-footer>
-    <div
-      class="row row-cols-2"
-      style="position: absolute; left: 0; top: 0; right: 0; bottom: 0"
-    >
-      <div class="col left-modal">
-        <div class="Frame-514">
-          <div class="LOGO">
-            <link
-              rel="stylesheet"
-              type="text/css"
-              href="http://fonts.googleapis.com/css?family=Ubuntu:regular,bold&subset=Latin"
-            />
-            <icon icon="logo" class="logo" style="height: 80px" />
-            <p class="sync-title">SYNC</p>
+  <b-modal
+    v-model="modalShow"
+    hide-header
+    hide-footer
+    body-class="p-0"
+    content-class="login-modal"
+  >
+    <b-container fluid class="h-100 p-0">
+      <b-row class="h-100" style="padding: 0 15px 0;">
+        <b-col class="left-modal">
+          <div class="content d-flex flex-column justify-content-between">
+            <div class="d-flex align-items-center">
+              <icon icon="logo" style="height: 80px" />
+              <strong class="title"> SYNC </strong>
+            </div>
+            <div style="height: 120px">
+              <ui class="h-100 d-flex flex-column justify-content-between">
+                <li
+                  v-for="slogan in slogans"
+                  :key="slogan"
+                  class="d-flex align-items-center"
+                >
+                  <icon icon="vector" style="height: 10px" />
+                  <div class="slogan"> {{ slogan }} </div>
+                </li>
+              </ui>
+            </div>
           </div>
-          <div class="Frame-511 Frame-text">
-            <icon icon="vector" class="icon" style="width: 14px" />
-            <p class="text1">一個匯集資訊的同步協作平台</p>
+        </b-col>
+
+        <b-col class="right-modal">
+          <b-button-close
+            style="margin: 1rem; position: absolute; top: 0; right: 0"
+            @click="modalShow = false"
+          />
+          <div class="content d-flex flex-column align-items-center">
+            <h3> 登入 </h3>
+            <b-button
+              v-for="login in thirdPartyLogins"
+              :key="login"
+              block
+              variant="light"
+              class="d-flex align-items-center"
+            >
+              <img :src="login.icon">
+              <div> {{ login.name }} 註冊/登入 </div>
+            </b-button>
+            <div class="split-line w-100 d-flex align-items-center"> 或 </div>
+            <b-form-group
+              v-for="(info, index) in loginInfos"
+              :key="index"
+              :label="info.name"
+              :label-for="`${info.name}-id`"
+              label-class="form-label"
+              class="form w-100"
+            >
+              <b-form-input
+                :id="`${info.name}-id`"
+                required
+                trim
+                :type="info.type"
+                :placeholder="info.name"
+              />
+            </b-form-group>
+            <div class="w-100">
+              <b-form-checkbox size="sm" class="keep-login-button">
+                保持登入狀態
+              </b-form-checkbox>
+            </div>
+            <b-button block style="background: #2353FF;"> 登入 </b-button>
+            <div class="option w-100 d-flex justify-content-between">
+              <b-link style="color: black;"> 忘記密碼？ </b-link>
+              <b-link style="color: #2353FF;"> 註冊新帳號 </b-link>
+            </div>
           </div>
-          <div class="Frame-512 Frame-text">
-            <icon icon="vector" class="icon" style="width: 14px" />
-            <p class="text2">新聞的多元觀點與客觀事實</p>
-          </div>
-          <div class="Frame-513 Frame-text">
-            <icon icon="vector" class="icon" style="width: 15px" />
-            <p class="text3">共同編輯新聞系統</p>
-          </div>
-        </div>
-      </div>
-      <div class="col right-modal">
-        <p>登入曲</p>
-      </div>
-    </div>
+        </b-col>
+      </b-row>
+    </b-container>
   </b-modal>
 </template>
 
@@ -50,7 +94,26 @@ export default {
       show: true,
       redirect: undefined,
       modalShow: true,
-      icons: ['vector-icon.svg', 'logo-icon.svg']
+      icons: ['vector-icon.svg', 'logo-icon.svg'],
+      slogans: [
+        '一個匯集資訊的同步協作平台',
+        '新聞的多元觀點與客觀事實',
+        '共同編輯新聞系統'
+      ],
+      thirdPartyLogins: [{
+        name: 'Facebook',
+        icon: 'https://img.icons8.com/color/24/000000/facebook-new.png'
+      }, {
+        name: 'Google',
+        icon: 'https://img.icons8.com/color/24/000000/google-logo.png'
+      }],
+      loginInfos: [{
+        type: 'text',
+        name: '帳號 / Email'
+      }, {
+        type: 'password',
+        name: '密碼'
+      }]
     }
   },
   watch: {
@@ -98,7 +161,9 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+$theme-colors: () !important;
+
 @media screen and (min-width: 0px) {
   .modal-dialog {
     max-width: 960px;
@@ -106,135 +171,111 @@ export default {
   }
 }
 
-.modal-content {
-  border-radius: 3%;
-}
-.left-modal {
-  border-top-left-radius: 3%;
-  border-bottom-left-radius: 3%;
-  background: linear-gradient(180deg, #03081a 0%, #0a194d 100%);
-}
-.right-modal {
-  border-top-right-radius: 3%;
-  border-bottom-right-radius: 3%;
-}
-.modal-header {
-  border-bottom: 0 none;
-  display: none;
-}
-.modal-body {
+::v-deep .login-modal {
   height: 720px;
-}
-.Frame-514 {
-  position: absolute;
-  width: 287.78px;
-  height: 274px;
-  left: 64px;
-  top: calc(50% - 274px / 2);
-  color: #ffffff;
-}
+  width: 960px;
+  top: calc(50% - 720px / 2);
+  left: calc(50% - 960px / 2);
+  overflow: hidden;
+  box-shadow: 0px 4px 25px rgba(0, 0, 0, 0.15);
+  border-radius: 16px;
 
-.Frame-511 {
-  width: 287.78px;
-  top: 144px;
-}
-.Frame-512 {
-  width: 271.77px;
-  top: 194px;
-}
-.Frame-513 {
-  width: 191.78px;
-  top: 244px;
-}
-.Frame-text {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  height: 30px;
-  left: 0px;
-}
-.text1 {
-  position: static;
-  width: 258px;
-  height: 30px;
-  padding: 10px;
-  /* Large Body / 18px - Medium */
-  font-family: 'Noto Sans CJK TC';
-  font-style: normal;
-  /* identical to box height, or 167% */
-  display: flex;
-  align-items: center;
-  letter-spacing: 2px;
+  .left-modal {
+    background: linear-gradient(to bottom, #03081a, #0a194d);
 
-  /* Gray/N_White */
-  color: #ffffff;
+    .content {
+      position: absolute;
+      height: 280px;
+      width: 300px;
+      top: calc(50% - 280px / 2);
+      left: calc(50% - 300px / 2);
 
-  /* Inside auto layout */
+      strong.title {
+        font-size: xxx-large;
+        letter-spacing: 5px;
+        color: white;
+        margin: 0 0 0 20px;
+        user-select: none;
+      }
 
-  margin: 8px;
-}
-.text2 {
-  position: static;
-  width: 238px;
-  height: 30px;
-  left: 0;
-  /* Large Body / 18px - Medium */
-  font-family: 'Noto Sans CJK TC';
-  font-style: normal;
-  /* identical to box height, or 167% */
-  display: flex;
-  align-items: center;
-  padding: 8px;
-  letter-spacing: 2px;
-  /* Gray/N_White */
-  color: #ffffff;
-  /* Inside auto layout */
+      .slogan {
+        font-size: large;
+        font-weight: normal;
+        letter-spacing: 2px;
+        color: white;
+        user-select: none;
+      }
+    }
+  }
 
-  margin: 10px;
-}
-.text3 {
-  position: static;
-  width: 158px;
-  height: 30px;
-  padding: 7px;
-  /* Large Body / 18px - Medium */
-  font-family: 'Noto Sans CJK TC';
-  font-style: normal;
+  .right-modal {
+    .content {
+      position: absolute;
+      width: 320px;
+      top: 20px;
+      left: calc(50% - 320px / 2);
 
-  /* identical to box height, or 167% */
-  display: flex;
-  align-items: center;
-  letter-spacing: 2px;
+      h3 {
+        margin : 20px 0;
+        font-weight: 700;
+        font-size: 24px;
+        line-height: 32px;
+      }
 
-  /* Gray/N_White */
-  color: #ffffff;
+      button {
+        height: 40px;
+        margin: 12px 0;
+        img {
+          position: absolute;
+          left: auto;
+        }
+        div {
+          font-size: 14px;
+          line-height: 24px;
+          margin: auto;
+        }
+      }
 
-  /* Inside auto layout */
-  flex: none;
-  order: 1;
-  flex-grow: 0;
-  margin: 10px;
-}
-.LOGO {
-  position: absolute;
-  width: 201.85px;
-  height: 64px;
-  left: calc(50% - 201.85px / 2 - 42.96px);
-  top: calc(50% - 64px / 2 - 105px);
-}
-.sync-title {
-  position: absolute;
-  left: 27.9%;
-  right: 0.84%;
-  top: 24.04%;
-  bottom: 20.67%;
-  letter-spacing: 9px;
-  font-weight: normal;
-  font-family: 'Ubuntu';
-  font-size: 1.2cm;
-  /* Gray/N_White */
-  color: #ffffff;
+      .split-line {
+        font-size: 14px;
+        line-height: 24px;
+        color: rgba(0, 0, 0, 0.45);
+
+        &::before,
+        &::after {
+          content: "";
+          flex: 1 1;
+          border-bottom: 1px solid;
+          color: rgba(0, 0, 0, 0.2);
+        }
+        &::before {
+          margin-right: 16px;
+        }
+        &::after {
+          margin-left: 16px;
+        }
+      }
+
+      .form{
+        margin: 12px 0;
+        .form-label{
+          font-size: 14px;
+          line-height: 24px;
+        }
+        .form-control{
+          border:1px solid #ced4da !important;
+          &:focus{
+            border-color: #80bdff !important;
+            box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25) !important;
+          }
+        }
+
+      }
+
+      .keep-login-button {
+        margin: 28px 0 0;
+      }
+    }
+  }
 }
 </style>
