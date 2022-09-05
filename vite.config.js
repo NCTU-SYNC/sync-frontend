fc03bce05b176a838e0e2aee9d77f93a5a02751d
newb1er <32424677+newb1er@users.noreply.github.com>
2022-09-06 01:04:21 +0800

    fix: scss bug

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,28 @@ import vue from '@vitejs/plugin-vue2'
 import path from "node:path"
 
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "@/assets/scss/custom.scss";
+        `
+      }
+    }
+  },
+
   plugins: [ vue() ],
+
 
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
+      "@": path.resolve(__dirname, "./src"),
     },
 
     extensions: ['.js', '.mjs', '.json', '.vue']
+  },
+
+  server: {
+    port: '8080'
   }
 })
