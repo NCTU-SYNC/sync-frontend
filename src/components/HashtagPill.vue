a353c7d2391ec7e5da5a78f8c8850606335e8df0
newb1er <32424677+newb1er@users.noreply.github.com>
2022-10-17 23:08:34 +0800

    [FE][SYNC-222] feat: new component 'HashtagPill'

---
+++ src/components/HashtagPill.vue
@@ -0,0 +1,37 @@
+<template>
+  <button @click.stop="GotoHashtag"># {{ name }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    GotoHashtag() {
+      this.$router.push({ path: '/hashtag', query: { q: this.name }})
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+  background-color: $gray-300;
+  border: none;
+  border-radius: 50px;
+  padding: 2px 8px;
+
+  cursor: pointer;
+  font: inherit;
+  line-height: inherit;
+
+  transition: background-color 0.15s ease-in-out;
+  &:hover {
+    background-color: $gray-400;
+  }
+}
+</style>
