77883a6b36829c0fd5ad6455292cc795ff2dde2e
newb1er <32424677+newb1er@users.noreply.github.com>
2022-11-08 00:06:11 +0800

    [SYNC-223][FE] fix: wrong prop type

--- src/components/AwaitEditCard.vue
+++ src/components/AwaitEditCard.vue
@@ -33,8 +33,8 @@ export default {
       default: 'title'
     },
     blocks: {
-      type: Object,
-      default: () => {}
+      type: Array,
+      default: () => []
     },
     lastUpdatedAt: {
       type: String,
