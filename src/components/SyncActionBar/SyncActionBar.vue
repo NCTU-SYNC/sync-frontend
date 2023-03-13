fb6704daa395ae09400138e439b07820e87cbc55
newb1er <taiwanmichael429@gmail.com>
2023-03-13 18:03:50 +0800

    [FE][SYNC-250] feat: action bar & action bar btn components

---
+++ src/components/SyncActionBar/SyncActionBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="action-bar__container">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+.action-bar__container {
+  display: flex;
+  gap: 1rem;
+  padding: 1rem 1.5rem;
+  background-color: white;
+  border-radius: 16px;
+  box-shadow: 0 4px 16px rgba(0, 0, 0, 0.16);
+
+  .action-bar__btn {
+    display: flex;
+    flex-direction: row;
+    gap: .25rem;
+    padding-right: 1rem;
+    border-right: 1px solid rgba(0, 163, 255, 0.3);
+
+    &:last-of-type {
+      border: 0;
+      padding-right: 0;
+    }
+
+    &:hover {
+      cursor: pointer;
+
+      .action-bar__text {
+        display: inline;
+        cursor: pointer;
+      }
+    }
+  }
+
+  .action-bar__text {
+    display: none;
+    color: $gray-8;
+    font-size: 0.875rem;
+    line-height: 1.5rem;
+  }
+}
+</style>
---
+++ src/components/SyncActionBar/SyncActionBarBtn.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="action-bar__btn" @click="handleClick">
+    <sync-icon :icon="icon" :size="iconSize" />
+    <span v-if="description.length > 0" class="action-bar__text">{{ description }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SyncActionBarBtn',
+  props: {
+    icon: {
+      type: String,
+      required: true
+    },
+    iconSize: {
+      type: String,
+      default: 'md'
+    },
+    description: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$emit('click')
+    }
+  }
+}
+</script>
